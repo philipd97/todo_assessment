763322a1989445bf5f3b7b4e9c9d6274796ea089
philipd97 <phildkh97@gmail.com>
2023-03-25 16:17:13 +0800

    add color gen and colors

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,48 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #FF7A00
+  static const Color importantText = Color(0xFFFF7A00);
+
+  /// Color: #FFDBC0
+  static const Color importantTile = Color(0xFFFFDBC0);
+
+  /// Color: #007A73
+  static const Color partiallyImportantText = Color(0xFF007A73);
+
+  /// Color: #B6FFF2
+  static const Color partiallyImportantTile = Color(0xFFB6FFF2);
+
+  /// Color: #00872E
+  static const Color todayCompletedText = Color(0xFF00872E);
+
+  /// Color: #ADFFC9
+  static const Color todayCompletedTile = Color(0xFFADFFC9);
+
+  /// Color: #E2E2E2
+  static const Color todayIncompleteTile = Color(0xFFE2E2E2);
+
+  /// Color: #0013C2
+  static const Color upcomingText = Color(0xFF0013C2);
+
+  /// Color: #C3C6FF
+  static const Color upcomingTile = Color(0xFFC3C6FF);
+
+  /// Color: #FF0000
+  static const Color veryImportantText = Color(0xFFFF0000);
+
+  /// Color: #FFDCDC
+  static const Color veryImportantTile = Color(0xFFFFDCDC);
+}

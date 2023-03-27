9faa4ed75797e71806f6de3f89136a1e297961d5
philipd97 <phildkh97@gmail.com>
2023-03-28 06:21:08 +0800

    add noglow behaviour and remove focus

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:todo_assessment/gen/fonts.gen.dart';
-import 'package:todo_assessment/helpers/sizer_helper.dart';
-import 'package:todo_assessment/views/starting_page.dart';
+import 'package:flutter/services.dart';
+
+import 'gen/fonts.gen.dart';
+import 'helpers/routes_helper.dart';
+import 'helpers/sizer_helper.dart';
+
+// TODO: add detail task screen/popup
+// TODO: add sqlite
+// TODO: add finished task
+// TODO: add task overlay
+// TODO: loading overlay
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(const MyApp());
 }
 
@@ -23,18 +33,25 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.grey,
             fontFamily: FontFamily.sofiaSans,
           ),
-          routerConfig: _routes,
+          routerConfig: routes,
+          builder: (context, child) => GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+            child: ScrollConfiguration(
+              behavior: NoGlowScrollBehaviour(),
+              child: child!,
+            ),
+          ),
         );
       },
     );
   }
 }
 
-final _routes = GoRouter(
-  routes: [
-    GoRoute(
-      path: StartingPage.routeName,
-      builder: (context, state) => const StartingPage(),
-    ),
-  ],
-);
+class NoGlowScrollBehaviour extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+}

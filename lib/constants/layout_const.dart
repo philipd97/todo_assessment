440848b3d811826e9c14b206a12a7f5e4c586adb
philipd97 <phildkh97@gmail.com>
2023-03-29 06:32:44 +0800

    add layout constants

---
+++ lib/constants/layout_const.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo_assessment/helpers/sizer_helper.dart';
+
+final taskDetailLabelStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontStyle: FontStyle.italic,
+  color: Colors.grey[500],
+  fontSize: 18.0,
+);
+
+const taskDetailTitleStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 38.0,
+  height: 0.0,
+);
+
+final sidePadding = EdgeInsets.symmetric(horizontal: 5.w);

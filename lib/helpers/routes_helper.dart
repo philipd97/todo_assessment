47b29e030db13ecef45d390cb5aac79d0be48778
philipd97 <phildkh97@gmail.com>
2023-03-30 12:29:18 +0800

    refactor routes and widgets

--- lib/helpers/routes_helper.dart
+++ lib/helpers/routes_helper.dart
@@ -1,55 +1,14 @@
-import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:todo_assessment/bloc/user_tasks_bloc.dart';
+import 'package:todo_assessment/model/task.dart';
 import 'package:todo_assessment/views/pages/home_page.dart';
+import 'package:todo_assessment/views/pages/previous_task_page.dart';
 import 'package:todo_assessment/views/pages/starting_page.dart';
 import 'package:todo_assessment/views/pages/task_detail_page.dart';
 import 'package:todo_assessment/views/pages/task_entry_page.dart';
 
-class GoRouterRefreshStream extends ChangeNotifier {
-  late final StreamSubscription _subscription;
-  String? username;
-
-  GoRouterRefreshStream(Stream<String?> stream) {
-    notifyListeners();
-    // _subscription = stream.asBroadcastStream().listen(
-    //   (name) {
-    //     log('name state: $name');
-    //     if (name == null) return;
-    //     username = name;
-    //     log('username: $username');
-    //     notifyListeners();
-    //   },
-    //   onDone: () {
-    //     if (username != null) {
-    //       _subscription.cancel();
-    //       log('stream cancelled');
-    //     }
-    //   },
-    // );
-  }
-
-  @override
-  void dispose() {
-    _subscription.cancel();
-    super.dispose();
-  }
-}
-
-// TODO: add animation page transition
 GoRouter routes(BuildContext context) {
   return GoRouter(
-    refreshListenable:
-        GoRouterRefreshStream(context.read<UserTaskBloc>().stream),
-    redirect: (context, state) {
-      final name = context.read<UserTaskBloc>().state;
-      if (name != null) return HomePage.routeName;
-      return null;
-    },
     routes: [
       GoRoute(
         path: StartingPage.routeName,
@@ -61,11 +20,19 @@ GoRouter routes(BuildContext context) {
       ),
       GoRoute(
         path: TaskDetailPage.routeName,
-        builder: (context, state) => const TaskDetailPage(),
+        builder: (context, state) => TaskDetailPage(
+          task: (state.extra as Map<String, dynamic>)['task'] as Task,
+        ),
       ),
       GoRoute(
         path: TaskEntryPage.routeName,
-        builder: (context, state) => const TaskEntryPage(),
+        builder: (context, state) => TaskEntryPage(
+          task: (state.extra as Map<String, dynamic>?)?['task'] as Task?,
+        ),
+      ),
+      GoRoute(
+        path: PreviousTaskPage.routeName,
+        builder: (context, state) => const PreviousTaskPage(),
       ),
     ],
   );
